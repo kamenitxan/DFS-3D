e1822f1a8975b47b53663109b1ad0af46f41df77
tomas.pavel@etnetera.cz <tomas.pavel@etnetera.cz>
2019-05-20 11:04:39 +0200

    .

--- src/main/java/cz/kamenitxan/df3d/pages/IndexPage.scala
+++ src/main/java/cz/kamenitxan/df3d/pages/IndexPage.scala
@@ -3,10 +3,12 @@ package cz.kamenitxan.df3d.pages
 import java.util
 
 import cz.kamenitxan.df3d.model.ProductService
+import cz.kamenitxan.jakon.core.configuration.Settings
 import cz.kamenitxan.jakon.core.customPages.AbstractCustomPage
 import cz.kamenitxan.jakon.core.model.Dao.DBHelper
 import cz.kamenitxan.jakon.core.template.TemplateUtils
 import cz.kamenitxan.jakon.webui.ObjectSettings
+
 import scala.collection.JavaConverters._
 
 class IndexPage extends AbstractCustomPage {
@@ -18,11 +20,13 @@ class IndexPage extends AbstractCustomPage {
 		val products = ProductService.getAll
 		conn.close()
 
+		Settings.setDefaultLocale("cs_CZ")
 		val model = new util.HashMap[String, AnyRef]()
 		model.put("products", products.asJava)
 		model.put("lang", "cs")
 		engine.render("index", "index", model)
 
+		Settings.setDefaultLocale("la_VA")
 		val latinModel = new util.HashMap[String, AnyRef]()
 		latinModel.put("products", products.asJava)
 		latinModel.put("lang", "la")
